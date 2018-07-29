488d4bb219d5db5808fa57d47db2a6efed60669d
JestemBartek <maciek.bartynski@gmail.com>
2018-07-29 19:19:21 +0200

    IE integrity

--- Zadanie1/main.js
+++ Zadanie1/main.js
@@ -5,6 +5,24 @@ function init() {
     paralax();
     btnToAsideBar();
     dynamicTxt();
+    dynamicTxtArticle();
+    btnsInsideAsideBar();
+}
+
+function btnsInsideAsideBar(){
+    let btns = document.querySelectorAll('.btnhead');
+    let articles = document.querySelectorAll('.article');
+    for (let i=0;i<2;i++){
+        let iter=i;
+        btns[iter].addEventListener('click', function(e){
+            articles[iter].classList.toggle('isActive');
+            if (iter===0){
+                articles[1].classList.remove('isActive')
+            }else{
+                articles[0].classList.remove('isActive')
+            }
+        })
+    }
 }
 
 function paralax() {
@@ -43,7 +61,7 @@ function btnToAsideBar() {
     let btn = document.querySelector('.button');
     let sidebar = document.querySelector('.sidebox');
     let fonticon = document.querySelector('.fonticon');
-    btn.addEventListener('click', function () {
+    btn.addEventListener('click', function () {   
         btn.classList.toggle('isClicked');
         sidebar.classList.toggle('isHidden');
         fonticon.classList.toggle('rotation');
@@ -79,4 +97,93 @@ function dynamicTxt() {
         }
     }
     setTextToParagraph(myname, mynameiterator, author);
+}
+
+function dynamicTxtArticle() {
+    let button = document.querySelector('#domcontentload');
+    button.addEventListener('click', dynamizeArticle);
+}
+function dynamizeArticle(){
+    document.querySelector('#domcontentload').removeEventListener('click', dynamizeArticle);
+    document.querySelector('#inn').classList.add('mainText');
+    setTimeout(iteratorStep, 400);
+    //iterator
+    let i = 0;
+    let array = ['m','y','t','e','x','t'];
+    function iteratorStep(){
+        let belt = document.querySelector('#a');
+        let iter = belt.querySelector('#iterator');
+        belt.classList.add('actualStep');
+        iter.innerText=i;
+        iter.classList.add('blink');
+        setTimeout(arrayStep, 600);
+    }
+    //array step
+    function arrayStep(){
+        document.querySelector('#a').classList.remove('actualStep');
+        document.querySelector('#a #iterator').classList.remove('blink');
+        let belt = document.querySelector('#c');
+        let char = belt.querySelector('#char');
+        belt.classList.add('actualStep');
+        char.innerText=array[i];
+        char.classList.add('blink');
+        setTimeout(innerTextStep, 600);
+    }
+    //innerTextStep
+    function innerTextStep(){
+        document.querySelector('#c').classList.remove('actualStep');
+        document.querySelector('#c #char').classList.remove('blink');
+        let belt = document.querySelector('#d');
+        let inn = document.querySelector('#inn');
+        belt.classList.add('actualStep');
+        inn.innerText += array[i];
+        setTimeout(iplusplusStep, 300);
+    }
+    //call i++
+    function iplusplusStep(){
+        document.querySelector('#d').classList.remove('actualStep');
+        document.querySelector('#d #inn').classList.remove('blink'); 
+        let belt = document.querySelector('#e');
+        belt.classList.add('actualStep');
+        belt.classList.add('blink');
+        setTimeout(iteratorPlus, 300);
+    }
+    //do i++
+    function iteratorPlus(){
+        document.querySelector('#e').classList.remove('actualStep');
+        document.querySelector('#e').classList.remove('blink');
+        let belt = document.querySelector('#a');
+        let iter = belt.querySelector('#iterator');
+        belt.classList.add('actualStep');
+        i++;
+        iter.innerText = i;
+        iter.classList.add('blink');
+        setTimeout(recursiveStep, 600);
+    }
+    //recursive
+    function recursiveStep(){
+        document.querySelector('#a').classList.remove('actualStep');
+        document.querySelector('#a').classList.remove('blink');
+        let belt = document.querySelector('#f');
+        belt.classList.add('actualStep');
+        let timer = document.querySelector('#dynamictxt i');
+        timer.classList.add('timerRotation');
+        setTimeout(callingNext, 1000);
+    }
+    //call function
+    function callingNext(){
+        document.querySelector('#call').classList.add('blink');
+        setTimeout(clearAll, 300);
+    }
+    function clearAll(){
+        document.querySelector('#f').classList.remove('actualStep');
+        document.querySelector('#f #call').classList.remove('blink');
+        document.querySelector('#f i').classList.remove('timerRotation');
+        if (i<6){
+            iteratorStep(); 
+        } else {
+            document.querySelector('#inn').classList.remove('mainText');
+            document.querySelector('#domcontentload').addEventListener('click', dynamizeArticle);
+        }
+    }
 }
